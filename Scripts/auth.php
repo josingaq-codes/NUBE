a309c55f3a79716a8781fb170218653e7a767bcf
JOSÉ LEONEL INGA QUINTANA <josingaq.codes@gmail.com>
2025-06-25 21:17:40 -0500

    v0.0.13

--- Scripts/auth.php
+++ Scripts/auth.php
@@ -4,7 +4,7 @@
 // Verificar si el usuario está autenticado
 if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] !== true) {
     // El usuario no está autenticado, redirigir a la página de inicio de sesión
-    header("Location: login.html");
+    header("Location: /html/login.html");
     exit(); // Asegura que el script se detenga después de la redirección
 }
 ?>
